b5fb8922cfa4f2dfdef43549c9dc9c317c4951b4
Júlio Nogueira <julio.nogueira@outlook.com>
2017-03-20 22:15:54 -0300

    Corrigindo a entidade servico.

--- src/main/java/br/com/tresmaria/ws/entity/Servico.java
+++ src/main/java/br/com/tresmaria/ws/entity/Servico.java
@@ -1,18 +1,39 @@
 package br.com.tresmaria.ws.entity;
 
+import java.util.Collection;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 public class Servico {
 
-	@javax.persistence.Id
+	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="Id")
 	private Long Id;
 	
+	@Basic(optional=false)
+	@NotNull
+	@Size(min=1, max = 300)
+	@Column(name="Descricao")
 	private String Descricao;
 
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="IdServico")
+	private Collection<ContatoXServico> contatoXServicoCollection;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="IdServico")
+	private Collection<Preco> precoCollection;
 	public Long getId() {
 		return Id;
 	}
@@ -29,6 +50,25 @@ public class Servico {
 		Descricao = descricao;
 	}
 	
+	
+	@XmlTransient
+	public Collection<ContatoXServico> getContatoXServicoCollection() {
+		return contatoXServicoCollection;
+	}
+	
+	public void setContatoXServicoCollection(Collection<ContatoXServico> contatoXServicoCollection) {
+		this.contatoXServicoCollection = contatoXServicoCollection;
+	}
+	
+	@XmlTransient
+	public Collection<Preco> getPrecoCollection() {
+		return precoCollection;
+	}
+
+	public void setPrecoCollection(Collection<Preco> precoCollection) {
+		this.precoCollection = precoCollection;
+	}
+
 	public Servico() {
 	}
 }
