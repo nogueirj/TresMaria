1dbf1b5eb72ced92447c4acb9785f7dc71095ebd
Júlio Nogueira <julio.nogueira@outlook.com>
2017-03-20 22:03:54 -0300

    retirado o nome de uma pasta perdida durante a renomeação das pastas.

--- src/main/java/br/com/tresmaria/ws/service/ServicoService.java
+++ src/main/java/br/com/tresmaria/ws/service/ServicoService.java
@@ -1,7 +1,5 @@
 package br.com.tresmaria.ws.service;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
