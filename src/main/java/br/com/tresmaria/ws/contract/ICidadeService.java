ab6e6c979d527fa50db9d9d080bab0ce7139b5a7
julio.nogueira <julio.nogueira@outlook.com>
2017-07-10 19:42:27 -0400

    Ajustado o contrato da cidade service

--- src/main/java/br/com/tresmaria/ws/contract/ICidadeService.java
+++ src/main/java/br/com/tresmaria/ws/contract/ICidadeService.java
@@ -1,5 +1,9 @@
 package br.com.tresmaria.ws.contract;
 
-public interface ICidadeService {
+import br.com.tresmaria.ws.model.CidadeDto;
 
+public interface ICidadeService {
+  public void salvar(CidadeDto dto);
+  public void alterar(CidadeDto dto);
+  public void excluir(Longong Id)
 }
